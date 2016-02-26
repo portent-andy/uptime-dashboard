732a255a7292fb35c270caf2be0dc614d1b288d3
Andy Schaff <aschaff@portentinteractive.com>
2016-02-26 15:14:45 -0600

    remove a domain properly for most OSes

--- deleteDomain.php
+++ deleteDomain.php
@@ -2,7 +2,7 @@
 if (isset($_POST['deleteDomainForm']) && !empty($_POST['deleteDomain'])) {
     $file = 'domains.txt';
     $current = file_get_contents($file);
-    $current = str_replace($_POST['deleteDomain'] . "\n", "", $current);
+    $current = str_replace($_POST['deleteDomain'] . "\r\n", "", $current);
     file_put_contents($file, $current);
 }
 ?>
